79e7b10e4ff48a20139fd3bd37b97e3b8856ef39
Frederick Wagner <doctaphred@gmail.com>
2019-01-30 15:19:50 -0500

    Add iTerm integration

--- __main__.sh
+++ __main__.sh
@@ -30,4 +30,4 @@ eval "$(direnv hook bash)"
 # export NVM_DIR="$HOME/.nvm"
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
-test -e "${HOME}/.iterm2_shell_integration.bash" && source "${HOME}/.iterm2_shell_integration.bash"
+source ~/profile/iterm.sh
