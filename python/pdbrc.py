810a0c8c9d99074a4648f4f342433b97cff24853
Frederick Wagner <doctaphred@gmail.com>
2019-03-14 14:45:49 -0400

    Add pdbrc

---
+++ python/pdbrc.py
@@ -0,0 +1,13 @@
+"""
+pdb++ configuration
+
+Symlink this file to ~/.pdbrc.py and pdb++ will pick it up:
+
+    ln -s ~/profile/python/pdbrc.py ~/.pdbrc.py
+"""
+import pdb
+
+
+class Config(pdb.DefaultConfig):
+    sticky_by_default = True  # start in sticky mode
+    current_line_color = 40  # black
