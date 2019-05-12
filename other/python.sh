6a05204dc43713b02a7a74c6502a32c421e60d5a
Frederick Wagner <doctaphred@gmail.com>
2019-05-12 10:43:31 -0400

    Tell virtualenvs not to mess with my prompt

--- other/python.sh
+++ other/python.sh
@@ -1,5 +1,8 @@
 export PYTHONSTARTUP=~/profile/python/startup.py
 
+# Tell virtualenvs not to mess with my prompt.
+export VIRTUAL_ENV_DISABLE_PROMPT=1
+
 alias ip='python3 -m IPython'
 alias ip3='python3 -m IPython'
 alias ip2='python2 -m IPython'
