bc01d8fc71bfc315ca5fb21be28a3de5041a880c
Frederick Wagner <doctaphred@gmail.com>
2019-05-16 17:34:23 -0400

    Add `pipdev` alias and requirements-dev.in

--- other/python.sh
+++ other/python.sh
@@ -6,3 +6,6 @@ export VIRTUAL_ENV_DISABLE_PROMPT=1
 alias ip='python3 -m IPython'
 alias ip3='python3 -m IPython'
 alias ip2='python2 -m IPython'
+
+# Install libraries I use for development in most projects.
+alias pipdev='pip install -r ~/profile/python/requirements-dev.in'
