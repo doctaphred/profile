83870c1376a02819a7cf82752c76b8dbe2df96b6
Frederick Wagner <doctaphred@gmail.com>
2019-05-20 08:39:07 -0400

    Fix SUBLIME_DIR

--- other/sublime.sh
+++ other/sublime.sh
@@ -1,4 +1,4 @@
-SUBLIME_DIR=~"/Library/Application Support/Sublime Text 3/Packages/User"
+SUBLIME_DIR="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
 
 proj() {
     subl --project "$SUBLIME_DIR/Projects/$1.sublime-project"
