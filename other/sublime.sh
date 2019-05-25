c4221d3b69500aa20b00735b534ece202659e46e
Frederick Wagner <doctaphred@gmail.com>
2019-05-25 13:45:14 -0400

    Improve proj

--- other/sublime.sh
+++ other/sublime.sh
@@ -3,11 +3,12 @@ SUBLIME_PROJECT_DIR="$SUBLIME_DIR/Projects"
 
 proj() {
     # Open a project in Sublime Text.
-
-    # Use a subshell so cd doesn't affect the parent directory.
     (
+        # cd to project directory to show only the basenames.
+        # Use a subshell so cd doesn't affect the parent directory.
         cd "$SUBLIME_PROJECT_DIR"
-        path="$(fzf-realpath $@)"
+        # TODO: Exclude extensions from display.
+        path="$(fd '\.sublime-project$' | fzf-realpath $@)"
         if test -n "$path"; then
             set -x
             # TODO: does the --project flag actually do anything?
