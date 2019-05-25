b61332cd6e98958c4f9e7fca96c780482a980257
Frederick Wagner <doctaphred@gmail.com>
2019-05-25 13:32:41 -0400

    Use fzf in `proj` function

--- other/sublime.sh
+++ other/sublime.sh
@@ -1,7 +1,19 @@
 SUBLIME_DIR="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+SUBLIME_PROJECT_DIR="$SUBLIME_DIR/Projects"
 
 proj() {
-    subl --project "$SUBLIME_DIR/Projects/$1.sublime-project"
+    # Open a project in Sublime Text.
+
+    # Use a subshell so cd doesn't affect the parent directory.
+    (
+        cd "$SUBLIME_PROJECT_DIR"
+        path="$(fzf-realpath $@)"
+        if test -n "$path"; then
+            set -x
+            # TODO: does the --project flag actually do anything?
+            subl --project "$path"
+        fi
+    )
 }
 
 alias prof='proj profile'
