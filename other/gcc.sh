5f55bceb8ae25c0addd23b7597081d0ed4b5da95
Frederick Wagner <doctaphred@gmail.com>
2019-05-25 20:14:13 -0400

    Update gcc.sh

--- other/gcc.sh
+++ other/gcc.sh
@@ -1,3 +1,10 @@
 # Make gcc use Homebrew openssl
 export LDFLAGS=-L/usr/local/opt/openssl/lib
 export CFLAGS=-I/usr/local/opt/openssl/include
+
+# Use gcc by default
+export CC=gcc-8
+export CXX=g++-8
+
+# Compile for this specific architecture (performance optimization)
+export CFLAGS="-march=native"
