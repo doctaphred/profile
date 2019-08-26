8f437d919615783b49a77476b3b019f3c4870bab
Frederick Wagner <doctaphred@gmail.com>
2019-08-26 15:09:05 -0400

    computers are a sadness and there is no cure

--- other/gcc.sh
+++ other/gcc.sh
@@ -2,9 +2,12 @@
 export LDFLAGS=-L/usr/local/opt/openssl/lib
 export CFLAGS=-I/usr/local/opt/openssl/include
 
-# Use gcc by default
-export CC=gcc-8
-export CXX=g++-8
+# XXX: Don't do this: way too many projects assume that if you're using
+# macOS, you're using clang.
+#
+# # Use gcc by default
+# export CC=gcc-8
+# export CXX=g++-8
 
 # TODO: combine this with the above CFLAGS?
 # # Compile for this specific architecture (performance optimization)
