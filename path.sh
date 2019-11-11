ddbdb38a0aab051ca3fbe9f132344aca9ef6b6d7
Frederick Wagner <doctaphred@gmail.com>
2019-11-11 12:15:04 -0500

    Update path.sh

--- path.sh
+++ path.sh
@@ -1,22 +1,12 @@
 # $PATH modifications
 
-# Systemd shell executable directory
-export PATH=~/.local/bin:$PATH
-
-# # GNU coreutils
-# PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-# MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
-
-# My personal executables (nonstandard locations)
-export PATH=~/bin:~/profile/bin:$PATH
-
-# Platform-specific executables (personal, nonstandard)
-# ex: ~/bin/"$(uname -s)"-"$(uname -m)" => ~/bin/Darwin-x86_64
-export PATH=~/bin/"$(uname -s)"-"$(uname -m)":~/bin/"$(uname -s)":$PATH
-
-use() {
+use () {
     export PATH="$(realpath "$1"):$PATH"
 }
 
-# Anaconda (anaconda.com)
-# export PATH=/usr/local/anaconda3/bin:"$PATH"
+# Systemd shell executable directory
+use ~/.local/bin
+
+# My personal executables (nonstandard locations)
+use ~/bin
+use ~/profile/bin
