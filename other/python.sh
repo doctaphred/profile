549d2c8b0f25998cbf84359af33904a071ca4086
Frederick Wagner <doctaphred@gmail.com>
2019-11-20 17:18:06 -0500

    Marginally restore sanity

--- other/python.sh
+++ other/python.sh
@@ -1,4 +1,13 @@
 export PYTHONSTARTUP=~/profile/python/startup.py
+# YES I UNDERSTAND THAT PYTHON 2.7 WILL REACH THE END OF ITS LIFE ON
+# JANUARY 1ST, 2020 THANK YOU FOR CONTINUALLY INFORMING ME
+export PYTHONWARNINGS='ignore:DEPRECATION::pip._internal.cli.base_command:'
+# Format is `action:message:category:module:line`. The `warnings` module
+# parses this value by just splitting it on ':', so we can't spell out
+# the full message, which includes a colon (ಠ_ಠ). To avoid accidentally
+# silencing other [less obnoxious and more helpful] warnings which also
+# happen to start with 'DEPRECATION', specify the module as well.
+
 
 # Tell virtualenvs not to mess with my prompt.
 export VIRTUAL_ENV_DISABLE_PROMPT=1
