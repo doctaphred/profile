e2242d91d8519c8c6f117e65e83e94c004e0a827
Frederick Wagner <doctaphred@gmail.com>
2019-12-15 11:27:42 -0500

    Print shell info on startup

--- __main__.sh
+++ __main__.sh
@@ -5,6 +5,8 @@
 # Style guide: https://google.github.io/styleguide/shell.xml
 
 set -e  # Exit immediately if there's an error.
+echo "Shell: $SHELL ($BASH_VERSION)"
+echo "PID: $BASHPID"
 
 # Get the directory containing this script, and resolve any symlinks.
 # (n.b. `BASH_SOURCE` is an array, but we just want the first element.)
