e288ee63f00c5932ee95d536917d293e9e776da4
Frederick Wagner <doctaphred@gmail.com>
2020-02-28 23:43:05 -0500

    Add a bunch more auto imports

--- python/startup.py
+++ python/startup.py
@@ -10,23 +10,59 @@ from collections import *
 from contextlib import *
 from functools import *
 from itertools import *
+from math import *  # Includes nan
+from operator import *
 from traceback import *
 
+import array
+import ast
+import bisect
 import collections
 import contextlib
+import copy
+import csv
+import decimal
+import difflib
 import functools
+import fractions
+import glob
+import hashlib
+import heapq
+import inspect
+import importlib
+import io
 import itertools
 import json
+import logging
+import operator
 import os
+import pdb
+import pickle
 import random
 import re
+import shutil
+import string
 import sys
 import time
+import timeit
 import traceback
 import traceback as tb
+import unicodedata
+import weakref
+import zipfile
 from datetime import datetime, timedelta
 from dis import dis
+from os import path
 from pprint import pprint
+from zipfile import ZipFile
+
+try:
+    import abc
+    import builtins
+    import enum
+    import statistics
+except ImportError:
+    pass
 
 try:
     from pathlib import Path
