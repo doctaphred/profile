3deb971245a4811c9423c011561623df44897053
Frederick Wagner <doctaphred@gmail.com>
2020-06-11 07:33:04 -0400

    Capitulate to corporate overlord

---
+++ other/dotnet.sh
@@ -0,0 +1,11 @@
+# > The .NET Core tools collect usage data in order to help us improve
+# > your experience. The data is anonymous. It is collected by Microsoft
+# > and shared with the community. You can opt-out of telemetry by
+# > setting the DOTNET_CLI_TELEMETRY_OPTOUT environment variable to '1'
+# > or 'true' using your favorite shell.
+# >
+# > Read more about .NET Core CLI Tools telemetry:
+# > https://aka.ms/dotnet-cli-telemetry
+#
+# This is the absolute worst thing I've ever heard of.
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
