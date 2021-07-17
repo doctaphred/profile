dbd59796150820c052340e55bfd556158bdb8afa
Frederick Wagner <frederick@frederickwagner.net>
2021-07-17 11:53:21 -0400

    Remove unused path

--- path.sh
+++ path.sh
@@ -20,7 +20,6 @@ unuse () {
 }
 
 # macOS applications
-try use "/Library/Frameworks/Python.framework/Versions/3.8"
 try use "/Applications/Visual Studio Code.app/Contents/Resources/app"
 
 # GNU find, locate, updatedb, and xargs
