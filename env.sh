76f4ad966f399210c08ad428af7c510b1fd19268
Frederick Wagner <frederick@frederickwagner.net>
2021-12-11 15:57:00 -0500

    Sort-of, partially, maybe mitigate CVE-2021-44228

--- env.sh
+++ env.sh
@@ -7,3 +7,6 @@ prohibit-subshells
 export NAME="Frederick Wagner"
 export EMAIL="doctaphred@gmail.com"
 export GITHUB_PERSONAL_ACCOUNT="doctaphred"
+
+# Just in case... (See https://logging.apache.org/log4j/2.x/security.html)
+export LOG4J_FORMAT_MSG_NO_LOOKUPS=true
